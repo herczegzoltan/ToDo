edd3a9f51bf34885bd85c0231608c344ee6697ee
DESKTOP-8N0UB1O\Zoltan <herczeg.zoltan92@gmail.com>
2022-04-01 11:31:07 +0200

    app-Add inital components and shared service, model

---
+++ ToDoApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoFormComponent } from './todo/todo-form/todo-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    TodoFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
