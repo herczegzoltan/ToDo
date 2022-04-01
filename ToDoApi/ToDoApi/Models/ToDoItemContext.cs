3d2cfca89400c7fb0d9d60e6130475a0063c0ffc
DESKTOP-8N0UB1O\Zoltan <herczeg.zoltan92@gmail.com>
2022-04-01 10:12:42 +0200

    api-Add ToDoItemContext and update Program.cs to use DbContext and include connectionstring

---
+++ ToDoApi/ToDoApi/Models/ToDoItemContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi.Models
+{
+    public class ToDoItemContext : DbContext
+    {
+        public ToDoItemContext(DbContextOptions<ToDoItemContext> options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItem { get; set; }
+    }
+}
